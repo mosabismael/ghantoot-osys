ecb11f3dd5a0b2131fdac655bdb800e325dd9e1a
mosabismael <mosabismael99@gmail.com>
2021-07-15 11:54:56 +0400

    upp

--- ui_sales_estimate/quotations_new.php
+++ ui_sales_estimate/quotations_new.php
@@ -36,6 +36,16 @@
 <!DOCTYPE html>
 <html dir="<?=$lang_dir; ?>" lang="<?=$lang; ?>">
 <head>
+<style>
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+th, td {
+  padding: 5px;
+  text-align: left;    
+}
+</style>
 	<?php include('app/meta.php'); ?>
     <?php include('app/assets.php'); ?>
 </head>
@@ -60,19 +70,19 @@ $get_client_id = $_GET['client_id'];
 id="new-quotation-form" 
 id-modal="add_new_quotation_modal" 
 api="<?=api_root; ?>sales/quotations/add_new.php">
-
 <div class="row">
 	<div class="col-100">
 	
 		<div class="form-grp">
-			<label><?=lang('Client_Name'); ?></label>
-			<select class="frmData" type="text" 
+			<label><?=lang('Account_Name'); ?></label>
+			<select class="frmData"
 					id="new-client_name" 
 					name="client_name" 
 					list="clients-data"
 					value="4"
 					req="1" 
 					den="" 
+					disabled
 					placeholder="<?=lang('Type Client Name to Select'); ?>" 
 					alerter="<?=lang("Please_Check_client_name", "AAR"); ?>">
 					<option value = "<?=$client_name; ?>" selected><?= $client_name; ?></option>
@@ -99,8 +109,110 @@ if(mysqli_num_rows($q_exe) > 0){
 	
 	
 </div>
+<div class="row">
+	<div class="col-50">
+		<div class="form-grp">
+<label><?=lang('Address'); ?></label>
+<input class="frmData" type="text" 
+					id="new-address" 
+					name="address" 
+					req="1" 
+					den="0" 
+					value=""
+					alerter="<?=lang("Please_Check_Address", "AAR"); ?>">
+</div>	
+</div>	
+
+<div class="col-50">
+<table style="width:100%">
+
+<tr>
+<tr>
+	  
+	  <th rowspan="2" >Prepared by</th>  
+	  <td><input class="frmData" type="text" 
+					id="new-prepared" 
+					name="prepared" 
+					req="1" 
+					den="0" 
+					value=""
+					alerter="<?=lang("Please_Check_prepared", "AAR"); ?>"></td>      
+	</tr>  
+	<tr>
+	</tr>
+  <tr>
+	  
+    <th rowspan="2" >AUTHORIZED SIGNATORY</th>  
+    <td><input class="frmData" type="text" 
+					id="new-AUTHORIZED_SIGNATORY" 
+					name="AUTHORIZED_SIGNATORY" 
+					req="1" 
+					den="0" 
+					value=""
+					alerter="<?=lang("Please_Check_AUTHORIZED_SIGNATORY", "AAR"); ?>"></td>      
+  </tr>  
+  <tr>
+    <td>Signature over Printed Name</td>
+  </tr>
+  <tr>
+    <th rowspan="2">CONFIRMED BY</th>
+    <td> 
+	<input class="frmData" type="text" 
+					id="new-CONFIRMED" 
+					name="CONFIRMED" 
+					req="1" 
+					den="0" 
+					value=""
+					alerter="<?=lang("Please_Check_CONFIRMED", "AAR"); ?>">
+	</td>
+  </tr>
+  <tr>
+    <td>Signature over Printed Name</td>
+  </tr>
+  <th rowspan="2" >Project Manager</th>  
+	  <td><input class="frmData" type="text" 
+					id="new-prepared" 
+					name="Project_Manager" 
+					req="1" 
+					den="0" 
+					value=""
+					alerter="<?=lang("Please_Check_Project_Manager", "AAR"); ?>"></td>      
+	</tr>  
+	<tr>
+	</tr>
+	<th rowspan="2" >Procurement Manager</th>  
+	  <td><input class="frmData" type="text" 
+					id="new-Procurement_Manager" 
+					name="Procurement_Manager" 
+					req="1" 
+					den="0" 
+					value=""
+					alerter="<?=lang("Please_Check_Procurement_Manager", "AAR"); ?>"></td>      
+	</tr>  
+	<tr>
+	</tr>
+</table>
+
+</div>	
+
+</div>
+<div class="row">
+	<div class="col-50">
 	
-	
+		<div class="form-grp">
+		<label><?=lang('Project_name'); ?></label>
+		<input class="frmData" type="text" 
+					id="new-Project_name" 
+					name="Project_name" 
+					req="1" 
+					den="0" 
+					value=""
+					alerter="<?=lang("Please_Project_name", "AAR"); ?>">
+</div>	
+</div>	
+
+</div>	
+
 <div class="row">
 	<div class="col-100" id="add_new_quotation_modal">
 		<div class="form-alerts" style="width: 50%;margin: 0 auto;text-align: left;"></div>
@@ -127,7 +239,7 @@ if(mysqli_num_rows($q_exe) > 0){
 					name="client_id" 
 					req="1" 
 					den="0" 
-					value="0"
+					value="<?=$get_client_id ?>"
 					alerter="<?=lang("Please_Check_Client_Name", "AAR"); ?>">
 
 
@@ -185,7 +297,6 @@ function loadClient(){
 				$('#payment_term_id').val(payment_term_id);
 				
 				$('#new-client_id').val(client_id);
-				console.log(response);
 
 				// $('#new-client_name').val(client_name);
 				if(client_id != '0'){
@@ -216,7 +327,66 @@ $("#new-client_name").on('input',function(){
 });
 
 
+window.onload = function exampleFunction() {
+	var client_name = $('#new-client_name').val().trim();
+	if(client_name != ''){
+		
+		$.ajax({
+		url      :"<?=api_root; ?>/clients/get_client_info.php",
+		data     :{'client_name': client_name, 'operation': 1},
+		dataType :"JSON",
+		type     :'POST',
+		success  :function(response){
+				var client_id = response[0].client_id;
+				var client_name = response[0].client_name;
+				var payment_term_id = response[0].payment_term_id;
+				payment_term_id = parseInt(payment_term_id);
+				$('#payment_term_id').val(payment_term_id);
+				
+				$('#new-client_id').val(client_id);
 
+				// $('#new-client_name').val(client_name);
+				if(client_id != '0'){
+					$('#clien_load_res').html('<span style="color:green;">Client Information Loaded</span>');
+					isClientSelected = true;
+					load_contacts();
+				} else {
+					isClientSelected = false;
+					$('#clien_load_res').html('<span style="color:red;">Client Information ERROR</span>');
+				}
+				if(payment_term_id == 0 && client_id != '0'){
+					alert('PLEASE UPDATE CLIENTS PAYMENT TERMS AT ACCOUNTING DEPARTMENT');
+					isClientSelected = false;
+					$('#clien_load_res').html('<span style="color:red;">Client Information ERROR, UPDATE PAYMENT TERM</span>');
+				}
+			},
+		error    :function(){
+			alert('Data Error No: 5467653');
+			isClientSelected = false;
+			},
+		});
+	}
+	var dd = parseInt($('#new-client_id').val());
+	if(dd != 0){
+		$('#added_contacts').html('LOADING DATA...');
+		$.ajax({
+		url      :"../app_api/clients/get_client_contacts.php",
+		data     :{'client_id': dd, 'operation': 1},
+		dataType :"html",
+		type     :'POST',
+		success  :function(data){
+			$('#added_contacts').html(data);
+			fix_cont_counters();
+			},
+		error    :function(){
+			alert('Data Error No: 5467653');
+			},
+		});
+				
+	} else {
+		
+	}
+        }
 
 function load_contacts(){
 	var dd = parseInt($('#new-client_id').val());
