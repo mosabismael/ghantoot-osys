f98785c98e0702a48a1e1e2327ad1999217b931a
mosabismael <mosabismael99@gmail.com>
2021-07-29 14:55:37 +0400

    fff

--- ui_sales_estimate/projects_estimate/add_level4_data.php
+++ ui_sales_estimate/projects_estimate/add_level4_data.php
@@ -7,6 +7,14 @@
 		$level3_id = $_GET['level3_id'];
 		$id = $_GET['id'];
 		$type_id = $_GET['type_id'];
+
+		$boq_qty = $_GET['item_qty-4'];
+		$complexity = $_GET['item_complexity-4'];
+		$boq_length = $_GET['item_length-4'];
+		$boq_price = $_GET['item_price-4'];
+		$boq_surfacearea = $_GET['item_surface_area-4'];
+		$type_id = $_GET['type_id'];
+
 		if($id==0){
 			$qu_project_level4_ins = "INSERT INTO `z_project_level4` (
 			`level4_name`, 
@@ -18,6 +26,23 @@
 			'".$level3_id."',
 			'".$prod_desc."',
 			'".$type_id."'
+			);";
+		}
+		$qu_z_boq_details_ins = "INSERT INTO `z_boq_details` (
+			`boq_name`, 
+			`boq_qty` ,
+			`complexity`,
+			`boq_length`,
+			`boq_surfacearea`,
+			`boq_price`
+			) VALUES (
+			'".$product_name."', 
+			'".$boq_qty."',
+			'".$complexity."',
+			'".$boq_length."',
+			'".$boq_surfacearea."',
+			'".$boq_price."'
+
 			);";
 		}
 		else{
